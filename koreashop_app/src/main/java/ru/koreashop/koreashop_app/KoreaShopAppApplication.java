5b14167ccd4d9480687fdd0c21163f9565dc2bd3
Vadim Marzan <vmandsa@mail.ru>
2022-12-05 13:02:15 +0400

    New project

---
+++ koreashop_app/src/main/java/ru/koreashop/koreashop_app/KoreaShopAppApplication.java
@@ -0,0 +1,13 @@
+package ru.koreashop.koreashop_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaShopAppApplication.class, args);
+	}
+
+}
---
+++ koreashop_app/src/test/java/ru/koreashop/koreashop_app/KoreashopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.koreashop.koreashop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreashopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
